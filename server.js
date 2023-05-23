5915e15b2d1647db4b037780129083732b204385
unknown <mobifone@DESKTOP-S59VM5C.mobifone.vn>
2023-05-23 15:43:47 +0700

    update nhanvien nghi viec

--- server.js
+++ server.js
@@ -5,6 +5,29 @@ const PORT = 5000;
 app.get("/", (req, res) => {
   res.send("Hello world");
 });
+app.get("/nhan-vien-nghi-viec", (req, res) => {
+  async function fetchDataCustomer() {
+    try {
+      const connection = await oracledb.getConnection({
+        user: "sale_owner",
+        password: "saleowner01122015",
+        connectString: "10.25.8.30/DB01",
+      });
+      const result = await connection.execute(
+        "SELECT * FROM sale_owner.NHAN_VIEN_NGHI_VIEC"
+      );
+      return result;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  fetchDataCustomer()
+    .then((dbRes) => res.send(dbRes))
+    .catch((err) => {
+      res.send(err);
+    });
+});
+
 app.get("/customers", (req, res) => {
   async function fetchDataCustomer() {
     try {
