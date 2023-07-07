156f854af04e5ecfa2d7cd070b6c86a2aa543854
unknown <mobifone@DESKTOP-S59VM5C.mobifone.vn>
2023-07-07 15:25:49 +0700

    update

--- src/app/controllers/ThaySim4GControllers.js
+++ src/app/controllers/ThaySim4GControllers.js
@@ -3,32 +3,46 @@ const DbConnection = require("../../DbConnection");
 class ThaySim4GController {
   index(req, res) {
     // Define the home page route
-    if ((req.query.type = "Dai ly")) {
-      console.log("req.query.type", req.query);
-    }
+
     const skip = req.query.skip;
     const limit = req.query.limit;
     const type = req.query.type;
     const month = req.query.month;
     const year = req.query.year;
+    const text = req.query.textSearch;
     let total = 0;
     if (skip && limit && month && year) {
       const monthFix = month < 10 ? "0" + month : month;
       const selectMonthYear = "01/" + monthFix + "/" + year;
-      const query = `SELECT * FROM sale_owner.CP_THAYSIM4G WHERE THANG_TT = TO_DATE(:selectMonthYear,'dd/mm/rrrr') 
+
+      const query = `SELECT isdn,  shop_code, shop_name,
+         shop_type, issue_datetime,emp_code,
+         emp_name, province, district_name, reason_id,
+         loai_tb, thang_tt
+          FROM sale_owner.CP_THAYSIM4G WHERE THANG_TT = TO_DATE(:selectMonthYear,'dd/mm/rrrr') 
             AND ISSUE_DATETIME >= TO_DATE(:selectMonthYear,'dd/mm/rrrr')
-             AND DAT_DK = 'DU DK' AND shop_type IN ( :type) offset :offsetbv rows fetch next :nrowsbv rows only`;
+             AND DAT_DK = 'DU DK' AND shop_type IN ( :type)  ${
+               text && text.length > 0 ? "and isdn like :querySearch" : ""
+             } offset :offsetbv rows fetch next :nrowsbv rows only`;
       const queryCount = `SELECT COUNT(*) FROM sale_owner.CP_THAYSIM4G WHERE THANG_TT = TO_DATE(:selectMonthYear,'dd/mm/rrrr') 
             AND ISSUE_DATETIME >= TO_DATE(:selectMonthYear,'dd/mm/rrrr')
-             AND DAT_DK = 'DU DK' AND shop_type IN ( :type) `;
+             AND DAT_DK = 'DU DK' AND shop_type IN ( :type)   ${
+               text && text.length > 0 ? "and isdn like :querySearch" : ""
+             }`;
+      console.log("query", query);
       DbConnection.getConnected(
         queryCount,
         {
           type: type,
           selectMonthYear: selectMonthYear,
+          ...(text && text.length > 0
+            ? { querySearch: `%` + text + `%` }
+            : null),
         },
         function (result) {
-          total = result[0][0];
+          if (result) {
+            total = result[0][0];
+          }
           DbConnection.getConnected(
             query,
             {
@@ -36,12 +50,15 @@ class ThaySim4GController {
               offsetbv: skip,
               type: type,
               selectMonthYear: selectMonthYear,
+              ...(text && text.length > 0
+                ? { querySearch: `%` + text + `%` }
+                : null),
             },
             function (data) {
               if (data) {
                 data.map((item, index) => {});
               }
-              console.log("data", data);
+              // console.log("data", data);
               res.send({ data: data, totalCount: total });
             }
           );
